e101b5e7f3947a06b9be23fdf845a0ed5797a7ce
Ahmad ashraf <ahmadashraf@Ahmads-MacBook-Pro-2.local>
2026-02-05 01:46:03 +0500

    manual trigger

---
+++ cmd/manual-trigger/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+
+	"github.com/twilight-project/forkoracle-go/address"
+	"github.com/twilight-project/forkoracle-go/comms"
+	db "github.com/twilight-project/forkoracle-go/db"
+	"github.com/twilight-project/forkoracle-go/transaction_signer"
+	btcOracleTypes "github.com/twilight-project/forkoracle-go/types"
+	utils "github.com/twilight-project/forkoracle-go/utils"
+)
+
+const (
+	HandlerSigningRefund = "signing_refund"
+	HandlerSigningSweep  = "signing_sweep"
+)
+
+var (
+	handler  string
+	runAll   bool
+	verbose  bool
+	showHelp bool
+)
+
+func init() {
+	flag.StringVar(&handler, "handler", "", "Handler to trigger: signing_refund or signing_sweep")
+	flag.BoolVar(&runAll, "all", false, "Run all signer handlers (signing_refund then signing_sweep)")
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose logging")
+	flag.BoolVar(&showHelp, "help", false, "Show help message")
+}
+
+func main() {
+	flag.Parse()
+
+	if showHelp {
+		printUsage()
+		os.Exit(0)
+	}
+
+	// Validate arguments
+	if !runAll && handler == "" {
+		fmt.Println("Error: Must specify --handler or --all")
+		printUsage()
+		os.Exit(1)
+	}
+
+	if handler != "" && handler != HandlerSigningRefund && handler != HandlerSigningSweep {
+		fmt.Printf("Error: Invalid handler '%s'. Must be '%s' or '%s'\n",
+			handler, HandlerSigningRefund, HandlerSigningSweep)
+		os.Exit(1)
+	}
+
+	// Initialize (reusing pattern from main.go)
+	accountName, signerAddr, dbconn := initialize()
+	defer dbconn.Close()
+
+	// Validate signer role
+	if err := validateSignerRole(signerAddr); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Register addresses on signers (as done in startTransactionSigner)
+	address.RegisterAddressOnSigners(dbconn)
+
+	// Execute handlers
+	if runAll {
+		fmt.Println("Running all signer handlers...")
+		runHandler(HandlerSigningRefund, accountName, dbconn, signerAddr)
+		runHandler(HandlerSigningSweep, accountName, dbconn, signerAddr)
+	} else {
+		runHandler(handler, accountName, dbconn, signerAddr)
+	}
+
+	fmt.Println("Manual trigger completed successfully")
+}
+
+func initialize() (string, string, *sql.DB) {
+	utils.InitConfigFile()
+	comms.GetAllFragments()
+	dbconn := db.InitDB()
+
+	signerAddr := viper.GetString("own_address")
+	accountName := viper.GetString("accountName")
+
+	// Validate running mode (warn if not signer)
+	runningMode := viper.GetString("running_mode")
+	if runningMode != "signer" {
+		fmt.Printf("Warning: running_mode is '%s', expected 'signer'. Proceeding anyway.\n", runningMode)
+	}
+
+	utils.LoadBtcWallet(viper.GetString("wallet_name"))
+
+	if verbose {
+		fmt.Printf("Initialized with:\n")
+		fmt.Printf("  Account Name: %s\n", accountName)
+		fmt.Printf("  Signer Address: %s\n", signerAddr)
+		fmt.Printf("  Running Mode: %s\n", runningMode)
+	}
+
+	return accountName, signerAddr, dbconn
+}
+
+func validateSignerRole(signerAddr string) error {
+	judgeAddress := viper.GetString("judge_address")
+	fragments := comms.GetAllFragments()
+
+	var fragment btcOracleTypes.Fragment
+	found := false
+	for _, f := range fragments.Fragments {
+		if f.JudgeAddress == judgeAddress {
+			fragment = f
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("no fragment found with judge address: %s", judgeAddress)
+	}
+
+	for _, signer := range fragment.Signers {
+		if signer.SignerAddress == signerAddr {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("signer %s is not registered with judge %s", signerAddr, judgeAddress)
+}
+
+func runHandler(handlerName string, accountName string, dbconn *sql.DB, signerAddr string) {
+	fmt.Printf("Executing handler: %s\n", handlerName)
+
+	switch handlerName {
+	case HandlerSigningRefund:
+		transaction_signer.ProcessTxSigningRefund(accountName, dbconn, signerAddr)
+	case HandlerSigningSweep:
+		transaction_signer.ProcessTxSigningSweep(accountName, dbconn, signerAddr)
+	default:
+		fmt.Printf("Unknown handler: %s\n", handlerName)
+	}
+
+	fmt.Printf("Handler %s completed\n", handlerName)
+}
+
+func printUsage() {
+	fmt.Println("manual-trigger - Manually trigger btc-oracle event handlers")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  manual-trigger --handler=<handler_name>")
+	fmt.Println("  manual-trigger --all")
+	fmt.Println()
+	fmt.Println("Handlers (for signer role):")
+	fmt.Println("  signing_refund  - Process unsigned refund transactions (triggered by unsigned_tx_refund event)")
+	fmt.Println("  signing_sweep   - Process unsigned sweep transactions (triggered by broadcast_tx_refund event)")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println("  manual-trigger --handler=signing_refund")
+	fmt.Println("  manual-trigger --handler=signing_sweep --verbose")
+	fmt.Println("  manual-trigger --all")
+	fmt.Println()
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+}
--- transaction_signer/transaction_signer.go
+++ transaction_signer/transaction_signer.go
@@ -18,11 +18,16 @@ func ProcessTxSigningSweep(accountName string, dbconn *sql.DB, signerAddr string
 	fmt.Println("starting Sweep Tx Signer")
 	wallet := viper.GetString("wallet_name")
 	btcPubKey := viper.GetString("btc_public_key")
-	SweepTxs := comms.GetAllUnsignedSweepTx()
+	fmt.Printf("[DEBUG] wallet: %s, btcPubKey: %s, signerAddr: %s\n", wallet, btcPubKey, signerAddr)
 
-	for _, tx := range SweepTxs.UnsignedTxSweepMsgs {
+	SweepTxs := comms.GetAllUnsignedSweepTx()
+	fmt.Printf("[DEBUG] Found %d unsigned sweep transactions\n", len(SweepTxs.UnsignedTxSweepMsgs))
+
+	for i, tx := range SweepTxs.UnsignedTxSweepMsgs {
 		reserveId, _ := strconv.Atoi(tx.ReserveId)
 		roundId, _ := strconv.Atoi(tx.RoundId)
+		fmt.Printf("\n[DEBUG] Processing sweep tx %d/%d: reserveId=%d, roundId=%d, judgeAddr=%s\n",
+			i+1, len(SweepTxs.UnsignedTxSweepMsgs), reserveId, roundId, tx.JudgeAddress)
 
 		for {
 			correspondingRefundTx := comms.GetBroadCastedRefundTx(uint64(reserveId), uint64(roundId))
@@ -36,29 +41,36 @@ func ProcessTxSigningSweep(accountName string, dbconn *sql.DB, signerAddr string
 		fmt.Printf("corresponding refund tx found  reserve Id: %d roundid : %d\n", reserveId, roundId)
 		// the Sweep tx sent to the chain is in Hex format
 		// encode it into base64 before passing to the decodePsbt function
-		sweepTx64, _ := utils.HexToBase64(tx.BtcUnsignedSweepTx)
+		sweepTx64, err := utils.HexToBase64(tx.BtcUnsignedSweepTx)
+		if err != nil {
+			fmt.Printf("[DEBUG] SKIP: error converting hex to base64: %v\n", err)
+			continue
+		}
 		decodedPsbt, err := comms.DecodePsbt(sweepTx64, wallet)
 		if err != nil {
-			fmt.Println("error decoding sweep tx : inside processSweepTx ->DecodePSBT : ", err)
+			fmt.Printf("[DEBUG] SKIP: error decoding sweep tx: %v\n", err)
 			continue
 		}
 
 		if len(decodedPsbt.Inputs) <= 0 {
-			fmt.Println("signing: no inputs")
+			fmt.Println("[DEBUG] SKIP: no inputs in decoded PSBT")
 			continue
 		}
 
 		script := decodedPsbt.Inputs[0].WitnessScript.Hex
+		fmt.Printf("[DEBUG] witness script hex: %s\n", script)
 
 		addresses := db.QueryUnsignedSweepAddressByScript(dbconn, script)
 		if len(addresses) <= 0 {
-			fmt.Println("signing: no address")
+			fmt.Printf("[DEBUG] SKIP: no address found in DB for script\n")
 			continue
 		}
 
 		reserveAddress := addresses[0]
+		fmt.Printf("[DEBUG] found address: %s, Signed_sweep: %v\n", reserveAddress.Address, reserveAddress.Signed_sweep)
 
 		if reserveAddress.Signed_sweep {
+			fmt.Printf("[DEBUG] SKIP: address already signed sweep\n")
 			continue
 		}
 
@@ -73,7 +85,7 @@ func ProcessTxSigningSweep(accountName string, dbconn *sql.DB, signerAddr string
 			}
 		}
 		if !found {
-			fmt.Println("No fragment found with the specified judge address")
+			fmt.Printf("[DEBUG] SKIP: No fragment found with judge address: %s\n", tx.JudgeAddress)
 			return
 		}
 
@@ -85,16 +97,18 @@ func ProcessTxSigningSweep(accountName string, dbconn *sql.DB, signerAddr string
 		}
 
 		if !found {
-			fmt.Println("Signer is not registered with the provided judge")
-		}
-
-		signatures, err := comms.SignPsbt(sweepTx64, wallet)
-		if err != nil {
-			fmt.Println("error signing psbt : inside processSweepTx ->SignPSBT: ", err)
+			fmt.Printf("[DEBUG] SKIP: Signer %s is not registered with judge %s\n", signerAddr, tx.JudgeAddress)
 			continue
 		}
 
-		fmt.Println("Sweep Signature : ", signatures)
+		fmt.Printf("[DEBUG] Signing PSBT with wallet: %s\n", wallet)
+		signatures, err := comms.SignPsbt(sweepTx64, wallet)
+		if err != nil {
+			fmt.Printf("[DEBUG] SKIP: error signing psbt: %v\n", err)
+			continue
+		}
+
+		fmt.Println("[DEBUG] Sweep Signature obtained: ", signatures)
 		cosmos := comms.GetCosmosClient()
 		msg := &bridgetypes.MsgSignSweep{
 			ReserveId:       uint64(reserveId),
@@ -104,12 +118,17 @@ func ProcessTxSigningSweep(accountName string, dbconn *sql.DB, signerAddr string
 			SignerAddress:   signerAddr,
 		}
 
+		fmt.Printf("[DEBUG] Sending MsgSignSweep to chain: reserveId=%d, roundId=%d, signerAddr=%s\n",
+			reserveId, roundId, signerAddr)
 		comms.SendTransactionSignSweep(accountName, cosmos, msg)
+		fmt.Printf("[DEBUG] MsgSignSweep sent successfully\n")
 
 		db.MarkAddressSignedSweep(dbconn, reserveAddress.Address)
+		fmt.Printf("[DEBUG] Marked address %s as signed sweep in DB\n", reserveAddress.Address)
 
 		// newAddress := comms.GetProposedSweepAddress(uint64(reserveId), uint64(roundId))
 		db.InsertTransaction(dbconn, decodedPsbt.Tx.TxID, reserveAddress.Address, uint64(reserveId), uint64(roundId))
+		fmt.Printf("[DEBUG] Inserted transaction into DB\n")
 	}
 	fmt.Println("finishing sweep tx signer")
 }
